e2e2c5bb5387ba1cfc0a80b954992fa9b80694a5
RodrigoERodriguez <rodrigoerodriguez48@gmail.com>
2024-04-29 11:42:34 -0300

    Creacion de los Componentes

---
+++ src/data/menuItems.ts
@@ -0,0 +1,18 @@
+import type { menuItem } from "../types/types";
+
+const menuItems: menuItem[] = [
+    {
+        title: 'Inicio',
+        url: '/',
+    },
+    {
+        title: 'Preguntas Frecuentes',
+        url: '/faq',
+    },
+    {
+        title: 'Blog',
+        url: '/blog',
+    },
+]
+
+export default menuItems;
